bc99515d91c7fa7df6d4b58a1e963c4ea38a4550
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-15 18:13:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,21 +9,21 @@ class Dog
     # self.save
   end
 
-  def self.all
-    @@all
-  end
-
-  def self.clear_all
-    @@all = []
-  end
-
-  def self.print_all
-    @@all.each do |dog|
-      puts dog.name
-    end
-  end
-
-  def self.save
-    @@all << self
-  end
+  # def self.all
+  #   @@all
+  # end
+  # 
+  # def self.clear_all
+  #   @@all = []
+  # end
+  # 
+  # def self.print_all
+  #   @@all.each do |dog|
+  #     puts dog.name
+  #   end
+  # end
+  # 
+  # def self.save
+  #   @@all << self
+  # end
 end
